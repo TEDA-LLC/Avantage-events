30217869c857b7b52d688290190a362a6a0c0107
Malikov Azizjon <80214840+MalikovAzizjon4843@users.noreply.github.com>
2023-02-01 17:39:17 +0500

    create project

---
+++ src/main/java/com/example/avantageevents/AvantageEventsApplication.java
@@ -0,0 +1,13 @@
+package com.example.avantageevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvantageEventsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AvantageEventsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/avantageevents/AvantageEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.avantageevents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvantageEventsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
