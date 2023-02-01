47f26ae058080837c9fd98e74b5c250c4e95db37
Malikov Azizjon <80214840+MalikovAzizjon4843@users.noreply.github.com>
2023-02-01 21:53:44 +0500

    ConstantRu added info

--- src/main/java/com/example/avantageevents/bot/constant/ConstantRu.java
+++ src/main/java/com/example/avantageevents/bot/constant/ConstantRu.java
@@ -4,4 +4,55 @@ package com.example.avantageevents.bot.constant;
  * @author Malikov Azizjon  *  01.02.2023  *  21:40   *  AvantageEvents
  */
 public interface ConstantRu {
+
+    String START = """
+            Здравствуйте! Добро пожаловать в наш бот!\s
+            Для начала выберите язык обслуживания:
+            """;
+    String BUTTON = "Русский \uD83C\uDDF7\uD83C\uDDFA";
+    String CONTACT_BUTTON = "Отправить контакт \uD83D\uDCDE";
+    String CONTACT = """
+            Введите свой номер телефона
+            \s
+            Отправить номер с помощью кнопки <ОТПРАВИТЬ КОНТАКТ>""";
+    String ABOUT_US_BUTTON = "О нас \uD83D\uDD0E";
+    String SERVICES_BUTTON = "Услуги \uD83D\uDEE0";
+    String EVENTS_BUTTON = "Выставки \uD83D\uDCBB";
+    String SETTINGS_BUTTON = "Настройки ⚙";
+    String VACANCY_BUTTON = "Вакансия \uD83D\uDC68\uD83C\uDFFB\u200D\uD83D\uDCBB";
+    String MENU = "Главное меню : ";
+    String ABOUT_US = """
+            <b>IBRAT Club\s
+            \s
+            </b>Улучшение жизни народа Узбекистана, которая развивается сегодня.
+            На этапе цифровой экономики оцифровка услуг многих предпринимателей и предложение их по доступной цене.
+
+            \uD83C\uDF10 <a href="https://ibrat.teda.uz/"> ibrat.teda.uz</a>
+            ☎ <b>+998 (93) 159 25 58</b>
+            \uD83D\uDCE7 <b>ibratclub@gmail.com</b>
+            \uD83D\uDCCD <b>Г. Термез, Байниминал, Фазилат улица 4/7</b>
+            \uD83D\uDCF8 <a href="https://www.instagram.com/ibrat_club/"> @ibrat_club</a>
+            <b>Оставьте свой вопрос нашему менеджеру и</b>
+            <b> наш менеджер ответит вам в ближайшее время.</b>
+            \uD83D\uDC64 <b>Менеджер : @ibrat_club_menejer</b>
+            """;
+    String VACANCY = "Вакансий пока нет \n" ;
+    //            "\uD83D\uDC64 Менеджер : @Proekt_Manager\n";
+    String LANGUAGE = "Изменить язык \uD83C\uDDF7\uD83C\uDDFA/\uD83C\uDDFA\uD83C\uDDFF/\uD83C\uDDEC\uD83C\uDDE7";
+    String PHONE = "Изменить номер \uD83D\uDCDE";
+    String BACK = "Назад ⬅";
+    String CHOOSE = "Выберите: ";
+    String EDITED = "Успешно изменен \uD83C\uDD97";
+    String CHOOSE_LANGUAGE = "Пожалуйста выберите язык:";
+    String LANGUAGE_ICON = "RUS \uD83C\uDDF7\uD83C\uDDFA";
+    String IN_DETAIL = "Подробно\uD83D\uDD0E";
+    String REQUEST = "Заявка\uD83D\uDCCC";
+    //    String RESPONSE_FOR_REQUEST= "Ваш запрос успешно получен.✅\n" +
+//            "Наш менеджер свяжется с вами в ближайшее время.";
+    String RESPONSE_FOR_REQUEST= "Ваш запрос успешно получен.✅\n" +
+            "";
+    String MY_REQUESTS = "Мои Заявки \uD83C\uDFAF";
+
+    String MY_REQUESTS_EMPTY = "У вас пока нет заявки";
+
 }
