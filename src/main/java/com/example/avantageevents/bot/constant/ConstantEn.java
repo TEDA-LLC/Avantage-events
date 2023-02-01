bd48011a6ebd67af76ad20beee16b3e87a4d791e
Malikov Azizjon <80214840+MalikovAzizjon4843@users.noreply.github.com>
2023-02-01 21:56:42 +0500

    ConstantEn added info

--- src/main/java/com/example/avantageevents/bot/constant/ConstantEn.java
+++ src/main/java/com/example/avantageevents/bot/constant/ConstantEn.java
@@ -5,6 +5,54 @@ package com.example.avantageevents.bot.constant;
  */
 public interface ConstantEn {
 
+    String START = """
+            Hello! Welcome to our bot!\s
+            First select the service language:
+            """;
+    String BUTTON = "English \uD83C\uDDEC\uD83C\uDDE7";
+    String CONTACT_BUTTON = "Share contact \uD83D\uDCDE";
+    String CONTACT = """
+            Enter your phone number
+            \s
+            Send number using <Share contact> button""";
+    String ABOUT_US_BUTTON = "About us \uD83D\uDD0E";
+    String SERVICES_BUTTON = "Services \uD83D\uDEE0";
+    String EVENTS_BUTTON = "Events \uD83D\uDCBB";
+    String SETTINGS_BUTTON = "Settings ⚙";
+    String VACANCY_BUTTON = "Vacancy \uD83D\uDC68\uD83C\uDFFB\u200D\uD83D\uDCBB";
+    String MENU = "Main menu : ";
+    String ABOUT_US = """
+            <b>IBRAT Club\s
+            \s
+            </b>Improving the life of the people of Uzbekistan, which is developing today.
 
+            At the stage of the digital economy, the services of many entrepreneurs are being digitalized and offered at an affordable price.
+
+            \uD83C\uDF10 <a href="https://ibrat.teda.uz/"> ibrat.teda.uz</a>
+            ☎ <b>+998 (93) 159 25 58</b>
+            \uD83D\uDCE7 <b>ibratclub@gmail.com</b>
+            \uD83D\uDCCD <b>Termiz C, Bayniminal district, Fazilat Street 4/7</b>
+            \uD83D\uDCF8 <a href="https://www.instagram.com/ibrat_club/"> @ibrat_club</a>
+            <b>Leave your question to our manager and </b>
+            <b> our manager will answer you as soon as possible.</b>
+            \uD83D\uDC64 <b> Manager : @ibrat_club_menejer</b>
+            """;
+    String VACANCY = "There are currently no vacancies\n" ;
+    //            "\uD83D\uDC64 Manager : @Proekt_Manager\n";
+    String LANGUAGE = "Change language \uD83C\uDDF7\uD83C\uDDFA/\uD83C\uDDFA\uD83C\uDDFF/\uD83C\uDDEC\uD83C\uDDE7";
+    String PHONE = "Change number \uD83D\uDCDE";
+    String BACK = "Back ⬅";
+    String CHOOSE = "Choose: ";
+    String EDITED = "Successfully modified \uD83C\uDD97";
+    String CHOOSE_LANGUAGE = "Choose language:";
+    String LANGUAGE_ICON = "ENG \uD83C\uDDEC\uD83C\uDDE7";
+    String IN_DETAIL = "In Detail\uD83D\uDD0E";
+    String REQUEST = "Request\uD83D\uDCCC";
+    //    String RESPONSE_FOR_REQUEST = "Your request has been successfully received.✅\n" +
+//            "Our manager will contact you soon.";
+    String RESPONSE_FOR_REQUEST = "Your request has been successfully received.✅\n" +
+            "";
+    String MY_REQUESTS = "My Requests \uD83C\uDFAF";
+    String MY_REQUESTS_EMPTY = "You don't have any requests";
 
 }
