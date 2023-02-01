f9ad9fe4282d6429aed7b7318debe1853f50c230
Malikov Azizjon <80214840+MalikovAzizjon4843@users.noreply.github.com>
2023-02-01 21:44:36 +0500

    ConstantUz added info

--- src/main/java/com/example/avantageevents/bot/constant/ConstantUz.java
+++ src/main/java/com/example/avantageevents/bot/constant/ConstantUz.java
@@ -5,6 +5,55 @@ package com.example.avantageevents.bot.constant;
  */
 public interface ConstantUz {
 
+    String START = """
+            Assalomu aleykum! Botimizga xush kelibsiz!\s
+            Xizmat ko’rsatish tilini tanlang:
+            """;
+    String BUTTON = "O'zbekcha \uD83C\uDDFA\uD83C\uDDFF";
+    String CONTACT_BUTTON = "Raqamni jo'natish \uD83D\uDCDE";
+    String CONTACT = "Telefon raqamingizni kiriting\n" +
+            " \n" +
+            "<RAQAMNI JO'NATISH> tugmasi yordamida raqamni yuboring";
+    String ABOUT_US_BUTTON = "Biz haqimizda \uD83D\uDD0E";
+    String SERVICES_BUTTON = "Xizmatlar \uD83D\uDEE0";
+    String EVENTS_BUTTON = "Tadbirlar \uD83D\uDCBB";
+    String SETTINGS_BUTTON = "Sozlamalar ⚙";
+    String VACANCY_BUTTON = "Vakansiya \uD83D\uDC68\uD83C\uDFFB\u200D\uD83D\uDCBB";
+    String MENU = "Asosiy menyu : ";
+    String ABOUT_US = """
+            <b>IBRAT Club\s
+            \s
+            </b>Hozirgi kunda rivojlanib borayotgan O'zbekiston xalqining hayotini yaxshilash.
+            Raqamli Iqtisodiyot bosqichida ko'plab tadbirkorlarning xizmatlarini elektronlashtirish va ularni qulay narxda taklif qilish.
 
+            \uD83C\uDF10 <a href="https://ibrat.teda.uz/"> ibrat.teda.uz</a>
+            ☎ <b>+998 (93) 159 25 58</b>
+            \uD83D\uDCE7 <b>ibratclub@gmail.com</b>
+            \uD83D\uDCCD <b>Termiz sh. Baynalminal mahalla,Fazilat ko`chasi 4/7</b>
+            \uD83D\uDCF8 <a href="https://www.instagram.com/ibrat_club/"> @ibrat_club</a>
+            <b>Menejirimizga savolingizni qoldiring va </b>
+            <b> menejerimiz sizga imkon qadar tezroq javob beradi.</b>
+            \uD83D\uDC64 <b> Manager : @ibrat_club_menejer</b>
+            """;
+
+    String VACANCY = "Hozircha vakansiya yo`q \n" ;
+    //            "\uD83D\uDC64 Manager : @Proekt_Manager\n";
+    String LANGUAGE = "Tilni o'zgartirish \uD83C\uDDF7\uD83C\uDDFA/\uD83C\uDDFA\uD83C\uDDFF/\uD83C\uDDEC\uD83C\uDDE7";
+    String PHONE = "Raqamni o'zgartirish \uD83D\uDCDE";
+    String BACK = "Orqaga ⬅";
+    String CHOOSE = "Tanlang: ";
+    String EDITED = "Muvafaqiyatli o'zgartirildi \uD83C\uDD97";
+    String CHOOSE_LANGUAGE = "Iltimos tilni tanlang:";
+    String LANGUAGE_ICON = "UZB \uD83C\uDDFA\uD83C\uDDFF";
+    String OK = "\uD83C\uDD97";
+    String IN_DETAIL = "Batafsil\uD83D\uDD0E";
+    String REQUEST = "So'rov\uD83D\uDCCC";
+    //    String RESPONSE_FOR_REQUEST = "Murojaatingiz muvafaqiyatli qabul qilindi.✅\n" +
+//            "Tez orada menejerimiz aloqaga chiqadi.";
+    String RESPONSE_FOR_REQUEST = "Murojaatingiz muvafaqiyatli qabul qilindi.✅\n" +
+            "";
+    String MY_REQUESTS = "Mening Murojaatlarim \uD83C\uDFAF";
+
+    String MY_REQUESTS_EMPTY = "Sizda hali hech qanday murojatlar mavjud emas";
 
 }
