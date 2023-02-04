c36173870c6f27b4460f076efbe3ab3b60760560
Malikov Azizjon <80214840+MalikovAzizjon4843@users.noreply.github.com>
2023-02-04 17:13:51 +0500

    address repo added

--- src/main/java/com/example/avantageevents/repository/AddressRepository.java
+++ src/main/java/com/example/avantageevents/repository/AddressRepository.java
@@ -1,7 +1,13 @@
 package com.example.avantageevents.repository;
 
+import com.example.avantageevents.model.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 /**
  * @author Malikov Azizjon  *  02.02.2023  *  18:15   *  AvantageEvents
  */
-public interface AddressRepository {
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address, Long> {
 }
