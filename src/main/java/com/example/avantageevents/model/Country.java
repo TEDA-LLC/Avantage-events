c7e18e166d6b2dd773df34ca147e9c1bd839614c
Malikov Azizjon <80214840+MalikovAzizjon4843@users.noreply.github.com>
2023-02-04 19:15:02 +0500

    country

---
+++ src/main/java/com/example/avantageevents/model/Country.java
@@ -0,0 +1,71 @@
+package com.example.avantageevents.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * @author Malikov Azizjon  *  04.02.2023  *  19:14   *  AvantageEvents
+ */
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@Entity
+@ToString
+public class Country {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Enumerated(EnumType.STRING)
+    private ActiveTypes activityType;
+    private String stirNumber;
+    @ManyToOne
+    private Company memberOrganization;
+    @ManyToOne(cascade = CascadeType.ALL)
+    private Address address;
+    @OneToMany(mappedBy = "company")
+    @ToString.Exclude
+    private List<BankInfo> bankInfo;
+    @ManyToOne
+    private Employee director;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime registeredTime = LocalDateTime.now();
+    @ManyToMany(fetch = FetchType.LAZY)
+    @ToString.Exclude
+    private List<User> clientList;
+    @OneToMany(mappedBy = "company")
+    @ToString.Exclude
+    private List<Bot> botList;
+    @ManyToMany
+    @ToString.Exclude
+    private List<Employee> employees;
+    @Builder.Default
+    private boolean active = true;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Attachment attachment;
+
+    public Company(Employee director) {
+        this.director = director;
+    }
+
+
+    public Company(String name, ActiveTypes activityType, String stirNumber, Address address, Company memberOrganization,List<User> employees, BankInfo bankInfo, Employee director) {
+        this.name = name;
+        this.activityType = activityType;
+        this.stirNumber = stirNumber;
+        this.address = address;
+        this.memberOrganization = memberOrganization;
+//        this.bankInfo = bankInfo;
+        this.director = director;
+    }
+
+}
