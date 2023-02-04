897895f947f2c3198101d7b7aaeb763a809eabaf
Malikov Azizjon <80214840+MalikovAzizjon4843@users.noreply.github.com>
2023-02-04 19:18:11 +0500

    active type entity

---
+++ src/main/java/com/example/avantageevents/model/ActiveTypes.java
@@ -0,0 +1,34 @@
+package com.example.avantageevents.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+/**
+ * @author Malikov Azizjon  *  04.02.2023  *  19:17   *  AvantageEvents
+ */
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+public class ActiveTypes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private ActiveTypes firstCase;
+    private ActiveTypes secondCase;
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime period = LocalDateTime.now();
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
+    @ToString.Exclude
+    private User client;
+
+}
