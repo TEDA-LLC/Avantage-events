c0f0cf5e6017e2946757f989c03faf50f5268cb4
Malikov Azizjon <80214840+MalikovAzizjon4843@users.noreply.github.com>
2023-02-06 13:25:44 +0500

    product(event)

---
+++ src/main/java/com/example/avantageevents/model/Product.java
@@ -0,0 +1,46 @@
+package com.example.avantageevents.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+/**
+ * @author Malikov Azizjon  *  06.02.2023  *  13:24   *  AvantageEvents
+ */
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(columnDefinition = "text")
+    private String nameUz, nameRu, nameEn, descriptionUz, descriptionRu, descriptionEn;
+
+    @ManyToOne
+    private Category category;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Attachment attachment;
+
+    private Double price;
+
+    private Integer minimumTerm;
+    //    @JsonFormat(pattern="yyyy-MM-dd")
+    private LocalDateTime fromDate, toDate;
+    @ManyToOne(cascade = CascadeType.ALL)
+    private Address address;
+
+    private String executionInterval;
+    @Builder.Default
+    @Column(nullable = true)
+    private boolean active = true;
+
+}
