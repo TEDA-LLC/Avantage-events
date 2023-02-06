95b26633502cb8f32f0d7608b307a377f640c9b4
Malikov Azizjon <80214840+MalikovAzizjon4843@users.noreply.github.com>
2023-02-06 13:07:33 +0500

    repositories optimization

--- src/main/java/com/example/avantageevents/repository/DistrictRepository.java
+++ src/main/java/com/example/avantageevents/repository/DistrictRepository.java
@@ -4,10 +4,13 @@ import com.example.avantageevents.model.District;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * @author Malikov Azizjon  *  04.02.2023  *  17:20   *  AvantageEvents
  */
 
 @Repository
 public interface DistrictRepository extends JpaRepository<District, Long> {
+    List<District> findAllByRegion_Id(Long regionId);
 }
--- src/main/java/com/example/avantageevents/repository/RegionRepository.java
+++ src/main/java/com/example/avantageevents/repository/RegionRepository.java
@@ -4,10 +4,13 @@ import com.example.avantageevents.model.Region;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * @author Malikov Azizjon  *  04.02.2023  *  19:10   *  AvantageEvents
  */
 
 @Repository
 public interface RegionRepository extends JpaRepository<Region, Long> {
+    List<Region> findAllByCountry_Id(Long countryId);
 }
