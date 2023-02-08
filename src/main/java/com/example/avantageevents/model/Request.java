cfaecb00800bde478153bfc0569bd048a2fe6ea5
Malikov Azizjon <80214840+MalikovAzizjon4843@users.noreply.github.com>
2023-02-08 14:24:12 +0500

    request entity

---
+++ src/main/java/com/example/avantageevents/model/Request.java
@@ -0,0 +1,42 @@
+package com.example.avantageevents.model;
+
+import com.example.avantageevents.model.enums.RegisteredType;
+import com.example.avantageevents.model.enums.RequestType;
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+/**
+ * @author Malikov Azizjon  *  08.02.2023  *  14:19   *  AvantageEvents
+ */
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+@Entity
+public class Request {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String aboutProduct, category;
+    private boolean view = false;
+    //    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime dateTime = LocalDateTime.now();
+    @ManyToOne
+    private Product product;
+    @Enumerated(EnumType.STRING)
+    private RequestType requestStatusType;
+    @ManyToOne
+    private User user;
+    @Enumerated(EnumType.STRING)
+    private RegisteredType registeredType;
+    @ManyToOne
+    private Employee employee;
+    private LocalDateTime arrivalTime;
+
+}
