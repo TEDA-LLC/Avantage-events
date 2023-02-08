3b2b5d040f5d065b6dc4818fea117ff620018111
Malikov Azizjon <80214840+MalikovAzizjon4843@users.noreply.github.com>
2023-02-08 14:22:08 +0500

    optimization

--- src/main/java/com/example/avantageevents/dto/ProductDTO.java
+++ src/main/java/com/example/avantageevents/dto/ProductDTO.java
@@ -1,5 +1,6 @@
 package com.example.avantageevents.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -20,6 +21,8 @@ public class ProductDTO {
     private Long categoryId;
     private AddressDTO address;
     private MultipartFile attachment;
+
+    @JsonFormat(pattern="yyyy-MM-ddTHH:mm")
     private LocalDateTime from, to;
     private Double price;
 
