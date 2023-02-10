8c48286734fcce75277c1f0d7a133268090ccf3e
Malikov Azizjon <80214840+MalikovAzizjon4843@users.noreply.github.com>
2023-02-10 10:29:47 +0500

    sort request fields added

---
+++ src/main/java/com/example/avantageevents/specification/SortRequest.java
@@ -0,0 +1,27 @@
+package com.example.avantageevents.specification;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author Malikov Azizjon  *  09.02.2023  *  23:22   *  AvantageEvents
+ */
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class SortRequest {
+
+    private String key;
+
+    private SortDirection direction;
+
+}
