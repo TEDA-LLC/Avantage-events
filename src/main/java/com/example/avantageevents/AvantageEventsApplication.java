0c663afb43c5ac2a4f1ee2dd6aee08c45e1d3a59
Malikov Azizjon <80214840+MalikovAzizjon4843@users.noreply.github.com>
2023-02-10 09:51:18 +0500

    changes region

--- src/main/java/com/example/avantageevents/AvantageEventsApplication.java
+++ src/main/java/com/example/avantageevents/AvantageEventsApplication.java
@@ -3,10 +3,13 @@ package com.example.avantageevents;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class AvantageEventsApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Tashkent"));
         SpringApplication.run(AvantageEventsApplication.class, args);
     }
 
