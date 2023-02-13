896e218d87d0c16ee84962c2d1d3506b179f7a04
Malikov Azizjon <80214840+MalikovAzizjon4843@users.noreply.github.com>
2023-02-13 16:25:31 +0500

    all Constants change information

--- src/main/java/com/example/avantageevents/bot/constant/ConstantUz.java
+++ src/main/java/com/example/avantageevents/bot/constant/ConstantUz.java
@@ -32,8 +32,8 @@ public interface ConstantUz {
             <b>Menejirimizga savolingizni qoldiring va </b>
             <b> menejerimiz sizga imkon qadar tezroq javob beradi.</b>
             \uD83D\uDC64 <b> Manager :info@avantage.events</b>\s
-            "\uD83C\uDFE6<b>Х/р: 20208000800895476001, РАКБ «Davr Bank» Mirzo Ulug`bek f-li Toshkent shahar</b>\n
-            "<b>МФО: 01072 ИНН: 305626506  ОКЭД: 82990</b>\s""";
+            \uD83C\uDFE6<b>Х/р: 20208000800895476001, РАКБ «Davr Bank» Mirzo Ulug`bek f-li Toshkent shahar</b>\s
+            <b>МФО: 01072 ИНН: 305626506  ОКЭД: 82990</b>\s""";
 
     String VACANCY = "Hozircha vakansiya yo`q \n" ;
     //            "\uD83D\uDC64 Manager : @Proekt_Manager\n";
