2d731d4cd25b3feaf8e1f48d0e5f3496da0b069e
AbdusamadMansurov <abdusamat.mansurov@gmail.com>
2023-02-13 14:05:02 +0500

    optimization for deploy

--- src/main/java/com/example/avantageevents/bot/constant/ConstantUz.java
+++ src/main/java/com/example/avantageevents/bot/constant/ConstantUz.java
@@ -33,10 +33,9 @@ public interface ConstantUz {
             \uD83D\uDCF8 <a href="https://www.instagram.com/avantage.events.agency/">@avantage.events.agency</a>
             <b>Menejirimizga savolingizni qoldiring va </b>
             <b> menejerimiz sizga imkon qadar tezroq javob beradi.</b>
-            \uD83D\uDC64 <b> Manager :info@avantage.events</b>\n
-            """ +
+            \uD83D\uDC64 <b> Manager :info@avantage.events</b>\s
             "\uD83C\uDFE6<b>Х/р: 20208000800895476001, РАКБ «Davr Bank» Mirzo Ulug`bek f-li Toshkent shahar</b>\n" +
-            "<b>МФО: 01072 ИНН: 305626506  ОКЭД: 82990</b>\n";
+            "<b>МФО: 01072 ИНН: 305626506  ОКЭД: 82990</b>\s""";
 
     String VACANCY = "Hozircha vakansiya yo`q \n" ;
     //            "\uD83D\uDC64 Manager : @Proekt_Manager\n";
